ae1f8797945e5437f12d3a395cd638ad6896abdc
Georgem Arruda <georgem.juniore@gmail.com>
2024-05-23 14:40:50 -0300

    Add files via upload

--- util/testUtil.go
+++ util/testUtil.go
@@ -1,11 +1,23 @@
 package testUtil
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func SetupEnv(envs map[string]string) {
 	for key, value := range envs {
 		os.Setenv(key, value)
 	}
+}
+
+func LoadEnv() error {
+	// Carrega as variáveis de ambiente do arquivo .env
+	err := godotenv.Load("c:\\workspace\\colabore-api\\.env")
+	if err != nil {
+		return fmt.Errorf("Erro ao carregar o arquivo .env: %v", err)
+	}
+	return nil
 }

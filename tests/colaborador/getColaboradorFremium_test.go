1118b22000f2d2abd8564071a1cfd188745c54c1
Ton Arruda <georgem.juniore@gmail.com>
2024-06-03 14:51:59 -0300

    Get endpoint Freemium

---
+++ tests/colaborador/getColaboradorFremium_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/patriciapersi/colabore-api/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetColaboradorFremium(t *testing.T) {
+
+	testCases := []struct {
+		description string
+		nrInsc      string
+		lastupdate  string
+		expected    int
+	}{
+		{
+			description: "Tentativa de buscar colaborador Freemium - Não Encontrado",
+			nrInsc:      "63542443",
+			lastupdate:  time.Now().Format("2006-01-02 15:04:05.000 -0700"),
+			expected:    http.StatusNotFound,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			client := config.SetupClient()
+			url := config.BaseURL + "/agente/Colaborador/Fremium"
+
+			queryParams := map[string]string{
+				"nrInscEmpregador": tc.nrInsc,
+				"lastupdate":       tc.lastupdate,
+			}
+
+			resp, err := client.R().
+				SetHeaders(config.SetupHeaders()).
+				SetQueryParams(queryParams).
+				Get(url)
+
+			assert.NoError(t, err, "Erro ao fazer a requisição")
+			assert.Equal(t, tc.expected, resp.StatusCode(), "Status de resposta inesperado")
+		})
+	}
+}

e81a64233f0592919bbae05f609c24ea4bccd5ac
Ton Arruda <georgem.juniore@gmail.com>
2024-06-04 08:53:03 -0300

    Ajuste Teste Definições

--- tests/definicoes/postDefinicoes_test.go
+++ tests/definicoes/postDefinicoes_test.go
@@ -2,50 +2,32 @@ package main
 
 import (
 	"net/http"
-	"os"
 	"testing"
 
 	"github.com/patriciapersi/colabore-api/config"
 	"github.com/stretchr/testify/assert"
-
-	testutil "github.com/patriciapersi/colabore-api/util"
 )
 
 func TestPostDefinicoes(t *testing.T) {
-
-	defer func() {
-		os.Clearenv()
-	}()
-
-	// Definindo uma tabela de casos de teste
 	testCases := []struct {
 		description string
-		envs        map[string]string // Mapa de variáveis de ambiente a serem configuradas
 		setupBody   bool
 		expected    int
 	}{
 		{
 			description: "Teste envio de Definições com sucesso",
-			envs: map[string]string{
-				"API_KEY": "ACTIjCHfsj1QZulkDK9oZ5bfPrlparVS44cb9o7F",
-			},
-			setupBody: true,
-			expected:  http.StatusOK,
+			setupBody:   true,
+			expected:    http.StatusOK,
 		},
 		{
 			description: "Teste envio de Definições sem body",
-			envs: map[string]string{
-				"API_KEY": "ACTIjCHfsj1QZulkDK9oZ5bfPrlparVS44cb9o7F",
-			},
-			setupBody: false,
-			expected:  http.StatusBadRequest,
+			setupBody:   false,
+			expected:    http.StatusBadRequest,
 		},
 	}
 
-	// Iterando sobre os casos de teste
 	for _, tc := range testCases {
 		t.Run(tc.description, func(t *testing.T) {
-			testutil.SetupEnv(tc.envs)
 
 			client := config.SetupClient()
 			req := client.R().

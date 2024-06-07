25271555c9ebef2d5e987a0ae6b7b666052ffef2
Ton Arruda <georgem.juniore@gmail.com>
2024-06-07 16:21:51 -0300

    Ajuste fixture

--- config/fixtures.go
+++ config/fixtures.go
@@ -6,12 +6,15 @@ import (
 	"github.com/google/uuid"
 )
 
+var nrInsc string = "10821992"
+var cpf string = "60515860409"
+
 // NewRequestBody retorna o corpo da requisição formatado conforme necessário.
 func DefinicoesRequestBody() map[string]interface{} {
 	return map[string]interface{}{
 		"Definicoes": []map[string]interface{}{
 			{
-				"NrInscEmpregador": "10821992",
+				"NrInscEmpregador": nrInsc,
 				"Ferias": map[string]interface{}{
 					"AntecedenciaMinima":     15,
 					"HabilitaFerias":         true,
@@ -26,13 +29,13 @@ func MensagensRequestBody() map[string]interface{} {
 	return map[string]interface{}{
 		"ID":               uuid.New().String(),
 		"TpInscEmpregador": "1",
-		"NrInscEmpregador": "10821992",
+		"NrInscEmpregador": nrInsc,
 		"MensagemTitulo":   "Teste automatizado",
 		"MensagemCorpo":    "Mensagem enviada pelo teste automatizado",
 		"DataMensagem":     time.Now().Format("02/01/2006"),
 		"Colaboradores": []map[string]interface{}{
 			{
-				"CPF": "60515860409",
+				"CPF": cpf,
 				"Contrato": map[string]interface{}{
 					"Matricula": "000031",
 					"Cargo":     "ALMOXARIFE",
@@ -45,8 +48,8 @@ func MensagensRequestBody() map[string]interface{} {
 func DeleteMensagensRequestBody(mensagemID string) map[string]interface{} {
 	return map[string]interface{}{
 		"MensagemId":       mensagemID,
-		"NrInscEmpregador": "10821992",
-		"ListaCPF":         []string{"60515860409"},
+		"NrInscEmpregador": nrInsc,
+		"ListaCPF":         []string{cpf},
 	}
 }
 
@@ -54,9 +57,9 @@ func PostInformacoesFeriasEmpregadoRequestBody() map[string]interface{} {
 	return map[string]interface{}{
 		"Colaboradores": []interface{}{
 			map[string]interface{}{
-				"CPF":                            "20306514940",
+				"CPF":                            cpf,
 				"NrInscEmpregador":               "10821992",
-				"Matricula":                      "000034",
+				"Matricula":                      "000031",
 				"SolicitouAdiantamento13":        false,
 				"DiasDisponiveisAbonoPecuniario": 10,
 				"DiasDisponiveis":                30,

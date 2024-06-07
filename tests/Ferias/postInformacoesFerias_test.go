abc5640b9d824855778067b39f9e32ebaf34cc0c
Ton Arruda <georgem.juniore@gmail.com>
2024-06-06 23:18:02 -0300

    Ajuste

--- tests/Ferias/postInformacoesFerias_test.go
+++ tests/Ferias/postInformacoesFerias_test.go
@@ -5,26 +5,40 @@ import (
 	"testing"
 
 	"github.com/patriciapersi/colabore-api/config"
+	testutil "github.com/patriciapersi/colabore-api/util"
 	"github.com/stretchr/testify/assert"
 )
 
 //Inserimos as definições para que o usuário possa solicitar férias
 
 func TestPostDefinicoesFerias(t *testing.T) {
+	if err := testutil.LoadEnv(); err != nil {
+		t.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+		t.Fatalf("%v", err)
+	}
 	testCases := []struct {
 		description string
 		setupBody   bool
+		header      map[string]string
 		expected    int
 	}{
 		{
 			description: "Teste envio de Definições com sucesso",
 			setupBody:   true,
+			header:      config.SetupHeaders(),
 			expected:    http.StatusOK,
 		},
 		{
 			description: "Teste envio de Definições sem body",
 			setupBody:   false,
+			header:      config.SetupHeaders(),
 			expected:    http.StatusBadRequest,
+		},
+		{
+			description: "Teste envio de Definições com header vazio",
+			setupBody:   true,
+			header:      map[string]string{},
+			expected:    http.StatusUnauthorized,
 		},
 	}
 
@@ -33,7 +47,7 @@ func TestPostDefinicoesFerias(t *testing.T) {
 
 			client := config.SetupClient()
 			req := client.R().
-				SetHeaders(config.SetupHeaders())
+				SetHeaders(tc.header)
 
 			// Configura o corpo da requisição se necessário
 			if tc.setupBody {
